3d839aa15fd3489f15ea1d5ffb16fa21a36002c5
Kirill Boblak <kaboblak@miem.hse.ru>
2020-11-24 19:49:01 +0300

    3.3. Разбор первой программы. [#417875]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kirill Boblak");
+        System.out.println("12.11.2000");
+    }
+}
