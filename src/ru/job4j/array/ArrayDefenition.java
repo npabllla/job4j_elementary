dfe3a8e0d90e3e8e1b2c6e39ae2cff71798a2b98
Kirill Boblak <kaboblak@miem.hse.ru>
2020-11-25 20:41:44 +0300

    6.0.2. Заполнение массива. [#417836]

--- src/ru/job4j/array/ArrayDefenition.java
+++ src/ru/job4j/array/ArrayDefenition.java
@@ -8,5 +8,14 @@ public class ArrayDefenition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Kirill";
+        names[1] = "Ilya";
+        names[2] = "Daniil";
+        names[3] = "Konstantin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
