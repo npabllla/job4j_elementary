1deb182f09f85a74cc5598481eaf4851446dd22c
Kirill Boblak <kaboblak@miem.hse.ru>
2020-11-25 16:42:00 +0300

    4.3.6. Math.abs [#417814]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x1-x2)==Math.abs(y1-y2)) {
+            return Math.abs(x2 - x1);
+        } else return 0;
+    }
+}
