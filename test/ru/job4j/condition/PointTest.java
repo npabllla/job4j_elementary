cbac94deb049603c1f1c30e43927eda82299b356
Kirill Boblak <kaboblak@miem.hse.ru>
2020-11-28 21:41:32 +0300

    4. Расстояние между точками в трехмерном пространстве. [#418013]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,6 +3,8 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.nio.channels.Pipe;
+
 import static org.junit.Assert.*;
 
 public class PointTest {
@@ -10,12 +12,8 @@ public class PointTest {
     @Test
     public void whenMinus56to38then8246() {
         double expected = 8.246;
-        int x1 = -5;
-        int y1 = 6;
-        int x2 = 3;
-        int y2 = 8;
-        Point first = new Point(x1, y1);
-        Point second = new Point(x2, y2);
+        Point first = new Point(-5, 6);
+        Point second = new Point(3, 8);
         double out = first.distance(second);
         Assert.assertEquals(out, expected, 0.01);
     }
@@ -23,12 +21,8 @@ public class PointTest {
     @Test
     public void when13Minus7to21then13601() {
         double expected = 13.601;
-        int x1 = 13;
-        int y1 = -7;
-        int x2 = 2;
-        int y2 = 1;
-        Point first = new Point(x1, y1);
-        Point second = new Point(x2, y2);
+        Point first = new Point(13, -7);
+        Point second = new Point(2, 1);
         double out = first.distance(second);
         Assert.assertEquals(out, expected, 0.01);
     }
@@ -36,13 +30,27 @@ public class PointTest {
     @Test
     public void when00to00then0() {
         double expected = 0;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
-        Point first = new Point(x1, y1);
-        Point second = new Point(x2, y2);
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 0);
         double out = first.distance(second);
         Assert.assertEquals(out, expected, 0.01);
+    }
+
+    @Test
+    public void when000to000then0() {
+        double expected = 0;
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(0, 0, 0);
+        double out = first.distance3d(second);
+        Assert.assertEquals(out, expected, 0.001);
+    }
+
+    @Test
+    public void when121620to101319hen0() {
+        double expected = 3.741;
+        Point first = new Point(12, 16, 20);
+        Point second = new Point(10, 13, 19);
+        double out = first.distance3d(second);
+        Assert.assertEquals(out, expected, 0.001);
     }
 }
